bd5080642ffbb606d2abf43e1e50c5eba35ee221
神楽坂雅詩 <cyscxchope@gmail.com>
2017-03-28 13:08:57 +0800

    建立工程

---
+++ YashiWebpageScreenshot/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace YashiWebpageScreenshot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webKitBrowser1 = new WebKit.WebKitBrowser();
+            this.SuspendLayout();
+            // 
+            // webKitBrowser1
+            // 
+            this.webKitBrowser1.BackColor = System.Drawing.Color.White;
+            this.webKitBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webKitBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webKitBrowser1.Margin = new System.Windows.Forms.Padding(0);
+            this.webKitBrowser1.Name = "webKitBrowser1";
+            this.webKitBrowser1.Size = new System.Drawing.Size(304, 281);
+            this.webKitBrowser1.TabIndex = 0;
+            this.webKitBrowser1.Url = null;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 281);
+            this.Controls.Add(this.webKitBrowser1);
+            this.Name = "Form1";
+            this.Text = "YashiWebpageScreenshot";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private WebKit.WebKitBrowser webKitBrowser1;
+    }
+}
+
